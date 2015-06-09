d32b6ed75a6794820be8713522a19a49064cd588
kele23 <scala233@gmail.com>
2015-06-09 10:19:36 +0200

    Vertex Cover Finale

--- makefile
+++ makefile
@@ -1,4 +1,19 @@
-vertexcover.x: vertexcover.cpp
+vertexcover_0.x: vertexcover_0.cpp
+	g++ -g -o $@ $< -std=c++11
+
+vertexcover_1.x: vertexcover_1.cpp
+	g++ -g -o $@ $< -std=c++11
+
+vertexcover_2.x: vertexcover_2.cpp
+	g++ -g -o $@ $< -std=c++11
+
+vertexcover_3.x: vertexcover_3.cpp
+	g++ -g -o $@ $< -std=c++11
+
+vertexcover_4.x: vertexcover_4.cpp
+	g++ -g -o $@ $< -std=c++11
+
+vertexcover_5.x: vertexcover_5.cpp
 	g++ -g -o $@ $< -std=c++11
 
 .PHONY: clean
--- vertexcover_finale.cpp
+++ vertexcover_finale.cpp
@@ -11,8 +11,6 @@
 /*------------------------------
 STRUTTURE UTILI
 ------------------------------*/
-#define WHITE 0
-#define BLACK 1
 
 class Vertex{
 
@@ -74,20 +72,12 @@ public:
 		this->vertexB = vertexB;
 	}
 
-	void setColor(int color){
-		this->color = color;
-	}
-
 	Vertex* getVertexA(){
 		return this->vertexA;
 	}
 
 	Vertex* getVertexB(){
 		return this->vertexB;
-	}
-
-	int getColor(){
-		return this->color;
 	}
 
 	int getTotalDegree(){
@@ -97,29 +87,25 @@ public:
 private:
 	Vertex* vertexA;
 	Vertex* vertexB;
-	int color = WHITE;
-};
-
-struct eComp  {
-	bool operator() (Edge* e1, Edge* e2) const {
-		return e1->getTotalDegree() > e2->getTotalDegree();
-	}
 };
 
 /*------------------------------
 END STRUTTURE UTILI
 ------------------------------*/
 
-void log(std::string,int V,Vertex** vertices,int E,Edge** edges,bool log_edges);
-void reload_weight(int V,Vertex** vertices,int E,bool use_degree);
-void merge_sort(Edge** edges,int start,int end);
+
+
+
 
 int main(int argc,char* argv[]){
 
 	int V,E;
 	Vertex** vertices;
 	Edge** edges;
-	std::set<Edge*, eComp> classifica;
+
+
+
+
 
 	/*------------------------------
 	LETTURA DA FILE
@@ -147,39 +133,28 @@ int main(int argc,char* argv[]){
 
 		Edge* edge = new Edge(vertices[vA],vertices[vB]);
 		edges[i] = edge;
-		classifica.insert(edge);
 	}
 
 	fclose(input);
 
-	//log("Lettura del Grafo",V,vertices,E,edges,false);
 	/*------------------------------
 	END LETTURA DA FILE
 	------------------------------*/
 
-	/*
 
-	for each edge e in E                        
-        Let epsilon = Min{W(v): v endpoint of E} 
-        W(v) -= epsilon for all v endpoint of E  
-    return {v: W(v)=0}
 
-	*/
+
+
+
 
 	/*------------------------------
 	COMPUTAZIONE RISULTATO
 	------------------------------*/
 
-
-	reload_weight(V,vertices,E,true);
-
-	//ALgoritmo KELE ( pesi a kele )
 	for(int i = 0; i < E; i++){
 		
 		Vertex* vA = edges[i]->getVertexA();
 		Vertex* vB = edges[i]->getVertexB();
-		
-		//int epsilon = vA->getWeight() < vB->getWeight() ? vA->getWeight() : vB->getWeight();
 		
 		Vertex* min = vA->getWeight() < vB->getWeight() ? vA : vB;
 		Vertex* max = min == vA ? vB : vA;
@@ -190,11 +165,14 @@ int main(int argc,char* argv[]){
 
 	}
 
-	//log("Uso dell'algoritmo KELE, modifica grado a rimozione di un arco ( Clever Greedy ) con pesi by Kele",V,vertices,E,edges,false);
-
 	/*------------------------------
 	END COMPUTAZIONE RISULTATO
 	------------------------------*/
+
+
+
+
+
 
 
 
@@ -221,112 +199,5 @@ int main(int argc,char* argv[]){
 	END SCRITTURA RISULTATO
 	------------------------------*/
 
-
-}
-
-void log(std::string title,int V,Vertex** vertices,int E,Edge** edges,bool log_edges){
-
-	static int log_number = 1;
-
-	std::cout << "-------------------------" << std::endl;
-	std::cout << "Log N° " << log_number++ << std::endl;
-	std::cout << title << std::endl;
-	std::cout << "-------------------------" << std::endl;
-
-	std::cout << "######### VERTICES ###########" << std::endl;
-
-	std::stringstream soluzione;
-
-	int solCount = 0;
-
-	for(int i = 0; i < V; i++){
-		std::cout << "V: " << i << "	W: " << vertices[i]->getWeight() << "	edges-> ";
-		std::unordered_set<int>* es = vertices[i]->getEdges();
-		if(es == NULL){
-			std::cout << "NONE";
-		}else{
-			for(int e : *es){
-				std::cout << e << " ";
-			}
-		}
-		std::cout << std::endl;
-
-		if(vertices[i]->getWeight() == 0){
-			solCount++;
-			soluzione << i << " ";
-		}
-	}
-
-	if(!soluzione.str().empty())
-		std::cout << "Soluzione ( " << solCount << " ) : " << soluzione.str() << std::endl;
-
-	if(log_edges){
-		std::cout << "######### EDGES ###########" << std::endl;
-		for(int i = 0; i < E; i++){
-			std::cout << "E: " << i << "	W: "<< edges[i]->getTotalDegree() << std::endl;
-		}
-	}
-
-}
-
-
-void reload_weight(int V,Vertex** vertices,int E,bool use_degree){
-
-	for(int i = 0; i < V; i++){
-
-		int degree = 0;
-		if(vertices[i]->getEdges() != NULL){
-			degree = vertices[i]->getEdges()->size();
-		}
-
-		if(use_degree)
-			vertices[i]->setWeight(E-degree);
-		else
-			vertices[i]->setWeight(E);
-
-	}
-}
-
-void merge(Edge** edges,int start,int k,int end){
-
-	Edge** temp = new Edge*[end-start+1];
-
-	int pLeft = start;
-	int pRight = k+1;
-
-	int i = 0;
-	while(pLeft <= k && pRight <= end){
-		if(edges[pLeft]->getTotalDegree() < edges[pRight]->getTotalDegree()){
-			temp[i++] = edges[pLeft++];
-		}else{
-			temp[i++] = edges[pRight++];
-		}
-	}
-
-	//Rimangono elementi sul lato sinistro
-	while(pLeft <= k){
-		temp[i++] = edges[pLeft++];
-	}
-
-	//Rimangono elementi sul lato destro
-	while(pRight <= end){
-		temp[i++] = edges[pRight++];
-	}
-
-	std::memcpy(&(edges[start]),temp,(end-start+1)*sizeof(Edge*));
-
-}
-
-void merge_sort(Edge** edges, int start, int end){
-
-	if(start >= end)
-		return;
-
-	int k = start+( (end-start)/2 );
-
-	merge_sort(edges,start,k);
-	merge_sort(edges,k+1,end);
-
-	merge(edges,start,k,end);
 
 }
